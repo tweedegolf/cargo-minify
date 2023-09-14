ff8210fb22604c88f684be6fb7bf36a430341f8c
Rick van der Wal <rick@tweedegolf.com>
2023-09-14 14:34:15 +0200

    Use cargo test instead of check

--- src/unused.rs
+++ src/unused.rs
@@ -21,7 +21,7 @@ pub fn get_unused<'a>(
 ) -> Result<impl Iterator<Item = UnusedDiagnostic> + 'a> {
     let mut command = Command::new("cargo");
 
-    command.args(["check", "--message-format", "json"]);
+    command.args(["test", "--message-format", "json"]);
 
     match crate_resolution {
         CrateResolutionOptions::Root => {}
